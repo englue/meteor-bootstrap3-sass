06d60790e6e1c6772dc4275f6fcd85487f920250
Sean Dwyer <sean.dwyer@gmail.com>
2014-01-21 20:15:28 -0800

    Add .editorconfig

--- package.js
+++ package.js
@@ -1,37 +1,37 @@
 Package.describe({
-    summary: "Bootstrap 3 with Sass support"
+	summary: "Bootstrap 3 with Sass support"
 });
 
 Package.on_use(function (api) {
-    api.add_files([
-        "assets/javascripts/bootstrap/affix.js",
-        "assets/javascripts/bootstrap/alert.js",
-        "assets/javascripts/bootstrap/button.js",
-        "assets/javascripts/bootstrap/carousel.js",
-        "assets/javascripts/bootstrap/collapse.js",
-        "assets/javascripts/bootstrap/dropdown.js",
-        "assets/javascripts/bootstrap/tab.js",
-        "assets/javascripts/bootstrap/transition.js",
-        "assets/javascripts/bootstrap/scrollspy.js",
-        "assets/javascripts/bootstrap/modal.js",
-        "assets/javascripts/bootstrap/tooltip.js",
-        "assets/javascripts/bootstrap/popover.js"
-    ], "client");
+	api.add_files([
+		"assets/javascripts/bootstrap/affix.js",
+		"assets/javascripts/bootstrap/alert.js",
+		"assets/javascripts/bootstrap/button.js",
+		"assets/javascripts/bootstrap/carousel.js",
+		"assets/javascripts/bootstrap/collapse.js",
+		"assets/javascripts/bootstrap/dropdown.js",
+		"assets/javascripts/bootstrap/tab.js",
+		"assets/javascripts/bootstrap/transition.js",
+		"assets/javascripts/bootstrap/scrollspy.js",
+		"assets/javascripts/bootstrap/modal.js",
+		"assets/javascripts/bootstrap/tooltip.js",
+		"assets/javascripts/bootstrap/popover.js"
+	], "client");
 
-    api.add_files([
-        "assets/fonts/bootstrap/glyphicons-halflings-regular.eot",
-        "assets/fonts/bootstrap/glyphicons-halflings-regular.svg",
-        "assets/fonts/bootstrap/glyphicons-halflings-regular.ttf",
-        "assets/fonts/bootstrap/glyphicons-halflings-regular.woff"
-    ], "client");
+	api.add_files([
+		"assets/fonts/bootstrap/glyphicons-halflings-regular.eot",
+		"assets/fonts/bootstrap/glyphicons-halflings-regular.svg",
+		"assets/fonts/bootstrap/glyphicons-halflings-regular.ttf",
+		"assets/fonts/bootstrap/glyphicons-halflings-regular.woff"
+	], "client");
 });
 
 Package.on_test(function(api) {
-    api.use("bootstrap3-sass");
-    api.use(["scss", "tinytest", "test-helpers"]);
+	api.use("bootstrap3-sass");
+	api.use(["scss", "tinytest", "test-helpers"]);
 
-    api.add_files([
-            "tests.scss",
-            "tests.js"
-        ], "client");
+	api.add_files([
+			"tests.scss",
+			"tests.js"
+		], "client");
 });
--- tests.js
+++ tests.js
@@ -1,39 +1,39 @@
 Tinytest.add("bootstrap3-sass - SCSS variables should be available", function(test) {
-    var onscreenDiv = OnscreenDiv(Meteor.render(function() {
-        return '<div class="test-screen-xs-width"></div>';
-    }));
+	var onscreenDiv = OnscreenDiv(Meteor.render(function() {
+		return '<div class="test-screen-xs-width"></div>';
+	}));
 
-    var styledDiv = onscreenDiv.div.firstChild;
+	var styledDiv = onscreenDiv.div.firstChild;
 
-    test.equal(getStyleProperty(styledDiv, "width"), "480px");
+	test.equal(getStyleProperty(styledDiv, "width"), "480px");
 
-    onscreenDiv.kill();
+	onscreenDiv.kill();
 });
 
 Tinytest.add("bootstrap3-sass - SCSS class should be extendable", function(test) {
-    var onscreenDiv = OnscreenDiv(Meteor.render(function() {
-        return '<a class="test-btn-extended"></a>';
-    }));
+	var onscreenDiv = OnscreenDiv(Meteor.render(function() {
+		return '<a class="test-btn-extended"></a>';
+	}));
 
-    var styledDiv = onscreenDiv.div.firstChild;
+	var styledDiv = onscreenDiv.div.firstChild;
 
-    test.equal(getStyleProperty(styledDiv, "display"), "inline-block");
-    test.equal(getStyleProperty(styledDiv, "text-align"), "center");
+	test.equal(getStyleProperty(styledDiv, "display"), "inline-block");
+	test.equal(getStyleProperty(styledDiv, "text-align"), "center");
 
-    onscreenDiv.kill();
+	onscreenDiv.kill();
 });
 
 Tinytest.add("bootstrap3-sass - jQuery extensions should be loaded", function(test) {
-    test.isTrue(typeof $.fn.affix !== 'undefined');
-    test.isTrue(typeof $.fn.alert !== 'undefined');
-    test.isTrue(typeof $.fn.button !== 'undefined');
-    test.isTrue(typeof $.fn.carousel !== 'undefined');
-    test.isTrue(typeof $.fn.collapse !== 'undefined');
-    test.isTrue(typeof $.fn.dropdown !== 'undefined');
-    test.isTrue(typeof $.fn.modal !== 'undefined');
-    test.isTrue(typeof $.fn.popover !== 'undefined');
-    test.isTrue(typeof $.fn.scrollspy !== 'undefined');
-    test.isTrue(typeof $.fn.tab !== 'undefined');
-    test.isTrue(typeof $.fn.tooltip !== 'undefined');
-    test.isTrue(typeof $.fn.emulateTransitionEnd !== 'undefined');
+	test.isTrue(typeof $.fn.affix !== 'undefined');
+	test.isTrue(typeof $.fn.alert !== 'undefined');
+	test.isTrue(typeof $.fn.button !== 'undefined');
+	test.isTrue(typeof $.fn.carousel !== 'undefined');
+	test.isTrue(typeof $.fn.collapse !== 'undefined');
+	test.isTrue(typeof $.fn.dropdown !== 'undefined');
+	test.isTrue(typeof $.fn.modal !== 'undefined');
+	test.isTrue(typeof $.fn.popover !== 'undefined');
+	test.isTrue(typeof $.fn.scrollspy !== 'undefined');
+	test.isTrue(typeof $.fn.tab !== 'undefined');
+	test.isTrue(typeof $.fn.tooltip !== 'undefined');
+	test.isTrue(typeof $.fn.emulateTransitionEnd !== 'undefined');
 });
