e82e70f60c1bc0d1867112e45ee401ccb9db6fa5
Sean Dwyer <sean.dwyer@gmail.com>
2014-08-29 16:01:13 -0700

    Upgrade to meteor 0.9

--- README.md
+++ README.md
@@ -6,11 +6,8 @@ Meteor package for Bootstrap 3 with Sass support
 To install
 ----------
 
-In addition to the bootstrap3-sass package, you must explicitly install the scss package. Meteor will not compile your `*.scss` files otherwise.
-
 ```sh
-$ mrt add bootstrap3-sass
-$ mrt add scss
+$ meteor add reywood:bootstrap3-sass
 ```
 
 To use
@@ -19,7 +16,7 @@ To use
 To activate bootstrap styles on your site, add the following line to the top of your main scss file:
 
 ```scss
-@import 'packages/bootstrap3-sass/bootstrap';
+@import '.meteor/local/build/programs/server/assets/packages/reywood:bootstrap3-sass/bootstrap';
 ```
 
 Be sure to add a viewport meta tag to your document `<head>` somewhere in your HTML. Where you add this is dependent on how your meteor project is structured.
--- package.js
+++ package.js
@@ -1,11 +1,18 @@
 Package.describe({
-	summary: "Bootstrap 3 with Sass support"
+	name: "reywood:bootstrap3-sass",
+	summary: "Bootstrap 3 with Sass support",
+	version: "3.1.5",
+	git: "https://github.com/reywood/meteor-bootstrap3-sass.git"
 });
 
-Package.on_use(function (api) {
-	api.use(["jquery", "scss"]);
+Package.onUse(function (api) {
+	api.versionsFrom("METEOR@0.9.0");
 
-	api.add_files([
+	api.use("jquery");
+	api.use("fourseven:scss@0.9.5", ["server"]);
+	api.imply("fourseven:scss", ["server"]);
+
+	api.addFiles([
 		"assets/javascripts/bootstrap/affix.js",
 		"assets/javascripts/bootstrap/alert.js",
 		"assets/javascripts/bootstrap/button.js",
@@ -20,19 +27,62 @@ Package.on_use(function (api) {
 		"assets/javascripts/bootstrap/popover.js"
 	], "client");
 
-	api.add_files([
+	api.addFiles([
 		"assets/fonts/bootstrap/glyphicons-halflings-regular.eot",
 		"assets/fonts/bootstrap/glyphicons-halflings-regular.svg",
 		"assets/fonts/bootstrap/glyphicons-halflings-regular.ttf",
 		"assets/fonts/bootstrap/glyphicons-halflings-regular.woff"
 	], "client");
+
+	api.addFiles([
+		"_bootstrap.scss",
+		"assets/stylesheets/bootstrap/_alerts.scss",
+		"assets/stylesheets/bootstrap/_badges.scss",
+		"assets/stylesheets/bootstrap/_breadcrumbs.scss",
+		"assets/stylesheets/bootstrap/_button-groups.scss",
+		"assets/stylesheets/bootstrap/_buttons.scss",
+		"assets/stylesheets/bootstrap/_carousel.scss",
+		"assets/stylesheets/bootstrap/_close.scss",
+		"assets/stylesheets/bootstrap/_code.scss",
+		"assets/stylesheets/bootstrap/_component-animations.scss",
+		"assets/stylesheets/bootstrap/_dropdowns.scss",
+		"assets/stylesheets/bootstrap/_forms.scss",
+		"assets/stylesheets/bootstrap/_glyphicons.scss",
+		"assets/stylesheets/bootstrap/_grid.scss",
+		"assets/stylesheets/bootstrap/_input-groups.scss",
+		"assets/stylesheets/bootstrap/_jumbotron.scss",
+		"assets/stylesheets/bootstrap/_labels.scss",
+		"assets/stylesheets/bootstrap/_list-group.scss",
+		"assets/stylesheets/bootstrap/_media.scss",
+		"assets/stylesheets/bootstrap/_mixins.scss",
+		"assets/stylesheets/bootstrap/_modals.scss",
+		"assets/stylesheets/bootstrap/_navbar.scss",
+		"assets/stylesheets/bootstrap/_navs.scss",
+		"assets/stylesheets/bootstrap/_normalize.scss",
+		"assets/stylesheets/bootstrap/_pager.scss",
+		"assets/stylesheets/bootstrap/_pagination.scss",
+		"assets/stylesheets/bootstrap/_panels.scss",
+		"assets/stylesheets/bootstrap/_popovers.scss",
+		"assets/stylesheets/bootstrap/_print.scss",
+		"assets/stylesheets/bootstrap/_progress-bars.scss",
+		"assets/stylesheets/bootstrap/_responsive-utilities.scss",
+		"assets/stylesheets/bootstrap/_scaffolding.scss",
+		"assets/stylesheets/bootstrap/_tables.scss",
+		"assets/stylesheets/bootstrap/_theme.scss",
+		"assets/stylesheets/bootstrap/_thumbnails.scss",
+		"assets/stylesheets/bootstrap/_tooltip.scss",
+		"assets/stylesheets/bootstrap/_type.scss",
+		"assets/stylesheets/bootstrap/_utilities.scss",
+		"assets/stylesheets/bootstrap/_variables.scss",
+		"assets/stylesheets/bootstrap/_wells.scss"
+	], "server");
 });
 
-Package.on_test(function(api) {
-	api.use("bootstrap3-sass");
-	api.use(["scss", "tinytest", "test-helpers"]);
+Package.onTest(function(api) {
+	api.use("reywood:bootstrap3-sass");
+	api.use(["fourseven:scss", "tinytest", "test-helpers"]);
 
-	api.add_files([
+	api.addFiles([
 			"tests.scss",
 			"tests.js"
 		], "client");
