2c754e345e763be7a27ac09cd23c9017c7113dfb
Sean Dwyer <sean.dwyer@gmail.com>
2014-10-04 12:05:33 -0700

    Bump version to 3.2.0_2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "reywood:bootstrap3-sass",
 	summary: "Bootstrap 3 with Sass support",
-	version: "3.2.0_1",
+	version: "3.2.0_2",
 	git: "https://github.com/englue/meteor-bootstrap3-sass.git"
 });
 
