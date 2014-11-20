4aea28916fb278434a5709fb5e05f749df31043a
Sean Dwyer <sean.dwyer@gmail.com>
2014-11-20 15:16:17 -0800

    Fix meteor version

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom("METEOR@1.0.0");
+	api.versionsFrom("METEOR@1.0");
 
 	api.use("jquery");
 	api.use("fourseven:scss@1.0.0", ["server"]);
